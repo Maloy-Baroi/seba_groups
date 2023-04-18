99df90cb3fc5fac2cb3b28c31fff82b4061668b8
unknown <maloy.virat@gmail.com>
2023-04-18 09:20:08 +0600

    pickup generic name from suggestion when adding product

--- ims_nextjs/src/pages/seller/components/CreateProductForm.js
+++ ims_nextjs/src/pages/seller/components/CreateProductForm.js
@@ -12,14 +12,45 @@ const CreateProductForm = () => {
     const [categories, setCategories] = useState([])
     const [subCategories, setSubCategories] = useState([])
     const [brand, setBrand] = useState([])
+    const [searchText, setSearchText] = useState("");
 
-    useEffect( () => {
-        const all_categories = getCategoryList();
-        const all_subCategories = getSubCategoryList();
-        const all_brands = getBrandList();
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const allCategories = await getCategoryList();
+            setCategories(allCategories);
+        };
 
-        console.log(all_categories)
-    }, [])
+        const fetchSubCategories = async () => {
+            const allSubCategories = await getSubCategoryList();
+            setSubCategories(allSubCategories);
+        };
+
+        const fetchBrands = async () => {
+            const allBrands = await getBrandList();
+            setBrand(allBrands);
+        };
+
+        fetchCategories().then(r => console.log("Category fetched"));
+        fetchSubCategories().then(r => console.log("Sub Category fetched"));
+        fetchBrands().then(r => console.log("Brand Fetched"));
+    }, []);
+
+    const filteredCategories = categories.filter((category) => {
+        return category.name.toLowerCase().startsWith(searchText.toLowerCase());
+    });
+
+    const handleInputChange = (event) => {
+        let generics = document.getElementById("genericID")
+        generics.style.display = "block";
+        setSearchText(event.target.value);
+    }
+
+    const handleCategorySelect = (categoryName) => {
+        setSearchText(categoryName);
+        let generics = document.getElementById("genericID")
+        generics.style.display = "none";
+    }
+
 
     return (
         <>
@@ -30,11 +61,22 @@ const CreateProductForm = () => {
                             <input type={"text"} className={"form-control"}/>
                         </div>
                         <div className={"col-md-3"}>
-                            <select className={"form-control"}>
-                                <option>
-                                    Choose Category
-                                </option>
-                            </select>
+                            <input type={"text"} className={"form-control"} value={searchText}
+                                   onChange={handleInputChange} placeholder={"Generic Name"}/>
+                            <ul id={"genericID"} style={
+                                searchText.length > 0 ?
+                                    {display: "block", height: "200px", overflow: "scroll"} :
+                                    {display: "none"}
+                            }
+                            >
+                                {filteredCategories.map((category) => (
+                                    <li key={category.id} onClick={() => handleCategorySelect(category.name)} style={{
+                                        cursor: "pointer"
+                                    }}>
+                                        {category.name}
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                         <div className={"col-md-3"}>
                             <select className={"form-control"}>
